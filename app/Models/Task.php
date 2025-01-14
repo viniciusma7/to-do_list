049c69f558a1a8cf20b22755a79dabd3604eabd0
vVinicisz <159086837+ViniBits@users.noreply.github.com>
2025-01-13 21:28:22 -0300

    Add Task model with relationships and fillable attributes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,25 @@ class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        "user_id",
+        "title",
+        "description",
+        "is_completed",
+        "date_limit",
+    ];
+
+    protected function casts()
+    {
+        return [
+            "is_completed" => "boolean",
+            "date_limit" => "date",
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
